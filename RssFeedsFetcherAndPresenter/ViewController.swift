8cc18e3d5db53c107b970067ae28bb50b000a2d0
Artur Azarau <artur.azarau@gmail.com>
2019-09-20 10:52:08 +0300

    Initial Commit

---
+++ RssFeedsFetcherAndPresenter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RssFeedsFetcherAndPresenter
+//
+//  Created by Артур Азаров on 20/09/2019.
+//  Copyright © 2019 Артур Азаров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
