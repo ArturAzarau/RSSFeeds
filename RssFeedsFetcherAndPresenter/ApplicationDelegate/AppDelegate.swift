cf994707dbd292fa929175027653a55ed01ef575
Artur Azarau <artur.azarau@gmail.com>
2019-09-20 11:01:37 +0300

    Initial project configuration

---
+++ RssFeedsFetcherAndPresenter/ApplicationDelegate/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  RssFeedsFetcherAndPresenter
+//
+//  Created by Артур Азаров on 20/09/2019.
+//  Copyright © 2019 Артур Азаров. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        let rootViewController = AllFeedsViewController()
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = rootViewController
+        window.makeKeyAndVisible()
+
+        return true
+    }
+}
+
