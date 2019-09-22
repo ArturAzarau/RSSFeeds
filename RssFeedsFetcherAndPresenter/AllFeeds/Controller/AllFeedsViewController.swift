007bd5013363f809232a022380cd0e3d0565ee8d
Artur Azarau <artur.azarau@gmail.com>
2019-09-22 18:55:44 +0300

    View controller now drives table view cells

--- RssFeedsFetcherAndPresenter/AllFeeds/Controller/AllFeedsViewController.swift
+++ RssFeedsFetcherAndPresenter/AllFeeds/Controller/AllFeedsViewController.swift
@@ -7,7 +7,36 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import SnapKit
 
 final class AllFeedsViewController: UIViewController {
-}
 
+    // MARK: - Properties
+
+    private let disposeBag = DisposeBag()
+    private let viewModel = AllFeedsViewModel()
+    private let tableView = AllFeedTableView()
+
+
+    // MARK: - Life Cycle
+
+    override func loadView() {
+        view = tableView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        viewModel.startFetchingRSSFeeds()
+
+        viewModel.viewModelsDriver
+            .drive(tableView.rx.items(cellIdentifier: FeedCell.reuseIdentifier,
+                                      cellType: FeedCell.self))
+            {(row, element, cell) in
+                cell.configure(with: element)
+            }
+            .disposed(by: disposeBag)
+    }
+}
