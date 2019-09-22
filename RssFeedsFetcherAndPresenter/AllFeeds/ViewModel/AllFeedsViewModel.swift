4ad840e7297d492bf2d226985d0aadf7e928b20a
Artur Azarau <artur.azarau@gmail.com>
2019-09-22 18:56:19 +0300

    Fetching and parsing rss feed was added to view model

--- RssFeedsFetcherAndPresenter/AllFeeds/ViewModel/AllFeedsViewModel.swift
+++ RssFeedsFetcherAndPresenter/AllFeeds/ViewModel/AllFeedsViewModel.swift
@@ -14,14 +14,43 @@ final class AllFeedsViewModel {
 
     // MARK: - Inner Types
 
+    private let disposeBag = DisposeBag()
     private let dataFetcher = DataFetcher()
-    private let viewModelsRelay = PublishRelay<[FeedCellViewModel]>()
+    private let viewModelsRelay = BehaviorRelay(value: [FeedCellViewModel]())
 
-    var viewModelsObservable: Observable<[FeedCellViewModel]> {
-        return viewModelsRelay.asObservable()
+    var viewModelsDriver: Driver<[FeedCellViewModel]> {
+        return viewModelsRelay.asDriver()
     }
 
     func startFetchingRSSFeeds() {
-        
+        guard let url = URL(string: "https://www.espn.com/espn/rss/news") else {
+            fatalError("Cannot construct proper url from string")
+        }
+
+        let rssParser = RSSParser()
+        rssParser.parseFeed(from: url)
+            .observeOn(MainScheduler.instance)
+            .subscribe(onSuccess: { [weak self] feedItems in
+                let viewModels = feedItems.map { [weak self] item -> FeedCellViewModel in
+                    let driver: Driver<UIImage?>
+                    if let self = self, let image = item.image {
+                        driver = self.dataFetcher
+                            .fetchImage(for: image)
+                            .map { UIImage(data: $0) }
+                            .asDriver(onErrorJustReturn: nil)
+                    } else {
+                        driver = Single.just(nil).asDriver(onErrorJustReturn: nil)
+                    }
+
+                    return FeedCellViewModel(title: item.title, description: item.description, imageDriver: driver)
+                }
+
+                self?.viewModelsRelay.accept(viewModels)
+            }, onError: { error in
+                print("Error: \(error)")
+            })
+            .disposed(by: disposeBag)
+
+
     }
 }
