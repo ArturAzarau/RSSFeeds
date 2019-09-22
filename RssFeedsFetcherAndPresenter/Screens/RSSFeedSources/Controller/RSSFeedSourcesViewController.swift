8e1f961f379aa45e6fff3a74276e611c59e566cc
Artur Azarau <artur.azarau@gmail.com>
2019-09-22 20:53:23 +0300

    New controller and view model were added

---
+++ RssFeedsFetcherAndPresenter/Screens/RSSFeedSources/Controller/RSSFeedSourcesViewController.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  RssFeedsFetcherAndPresenter
+//
+//  Created by Артур Азаров on 22/09/2019.
+//  Copyright © 2019 Артур Азаров. All rights reserved.
+//
+
+import UIKit
+
+
+final class RSSFeedSourcesViewController: UIViewController {
+    
+}
---
+++ RssFeedsFetcherAndPresenter/Screens/RSSFeedSources/ViewModel/RSSFeedSourcesViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  RSSFeedSourcesViewModel.swift
+//  RssFeedsFetcherAndPresenter
+//
+//  Created by Артур Азаров on 22/09/2019.
+//  Copyright © 2019 Артур Азаров. All rights reserved.
+//
+
+import Foundation
+
+final class RSSFeedSourcesViewModel {
+    
+}
